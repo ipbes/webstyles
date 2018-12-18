158877a8b1c2ae8e686a48e3122cce83f2fb1f4e
Benedict Omare <benaboki@gmail.com>
2018-12-18 16:31:35 +0100

    fix template.php

--- template.php
+++ template.php
@@ -10,12 +10,21 @@ foreach (glob($includes_path) as $filename) {
   require_once dirname(__FILE__) . '/includes/' . basename($filename);
 }
 
-/**
- * Implements template_preprocess_page().
- */
-function ipbes_new_preprocess_page(&$variables) {
-  // Add copyright to theme.
-  if ($copyright = theme_get_setting('copyright')) {
-    $variables['copyright'] = check_markup($copyright['value'], $copyright['format']);
+function ipbes_new_form_views_exposed_form_alter(&$form, &$form_state, $form_id) {
+  if (isset($form['search_api_views_fulltext'])) {
+    $form['search_api_views_fulltext']['#attributes']['placeholder'] = t('Enter search terms');
   }
+   if (isset($form['combine'])) {
+      $form['combine']['#attributes']['placeholder'] = t('Enter search terms');}
 }
+
+function ipbes_new_preprocess_views_exposed_form(&$vars, $hook) {
+    // Change the text on the submit button
+    $vars['form']['submit']['#value'] = t('Search');
+
+    // Rebuild the rendered version (submit button, rest remains unchanged)
+    unset($vars['form']['submit']['#printed']);
+    $vars['button'] = drupal_render($vars['form']['submit']);
+}
+
+?>
